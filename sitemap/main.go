a351b0e7d7e316e764562014ce15c78408e9d5f6
zbrill <c.hits@yahoo.com>
2024-04-08 22:59:29 -0700

    absolute paths supported, no relative paths yet

--- sitemap/main.go
+++ sitemap/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"io"
-	"link"
 	linklib "link"
 	"log"
 	"net/http"
 	urlpkg "net/url"
+	"os"
 )
 
 /**
-findAllLinks that takes a url and finds all links accessible from that url to a max depth
+buildMap that takes a url and finds all links accessible from that url to a max depth
  - list of links generated for landing url (flag set by user on command line)
- - iterate over list of links and do findAllLinks on each?
-	- pass depth to this findAllLinks which returns if depth > depth
+ - iterate over list of links and do buildMap on each?
+	- pass depth to this buildMap which returns if depth > depth
 
 can use a map as a set in go. set of all urls makes sure there are no duplicates and acts
 as a check to say 'have i visited this site already'
@@ -30,20 +31,30 @@ var maxDepth int
 var url string
 var baseURL string
 
+const xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+type loc struct {
+	Value string `xml:"loc"`
+}
+
+type urlset struct {
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
+}
+
 func main() {
 	u, d := parseFlags()
 	url = u
 	baseURL = getURLRoot(url)
 	maxDepth = d
-	allLinks := findAllLinks(make([]linklib.Link, 0), make(map[linklib.Link]bool), 0)
-	fmt.Println(allLinks)
-	// sitemap = writeXML(allLinks)
-	// fmt.Println(sitemap)
+	allLinks := buildMap(make([]linklib.Link, 0), make(map[linklib.Link]bool), 0)
+	xml := toXml(allLinks)
+	writeXML(xml)
 }
 
 func parseFlags() (string, int) {
 	var url = flag.String("url", "https://github.com", "URL for the site to build sitemap from")
-	var depth = flag.Int("depth", 4, "maximum recursive depth")
+	var depth = flag.Int("depth", 1, "maximum recursive depth")
 	flag.Parse()
 	return *url, *depth
 }
@@ -63,44 +74,53 @@ func getHTML(url string) []byte {
 func getURLRoot(urlStr string) string {
 	u, err := urlpkg.Parse(urlStr)
 	if err != nil {
-		panic(err)
+		// fmt.Println("couldn't parse ", urlStr)
+		return ""
 	}
 	u.Path = ""
 	u.RawQuery = ""
 	u.Fragment = ""
-	fmt.Println(u.Host)
+	u.Scheme = ""
 	return u.String()
 }
 
-func findAllLinks(links []linklib.Link, seen map[linklib.Link]bool, currdepth int) []link.Link {
+func buildMap(links []linklib.Link, seen map[linklib.Link]bool, currdepth int) []linklib.Link {
 	if currdepth > maxDepth {
-		return links
+		var empty []linklib.Link
+		return empty
 	}
 	if currdepth == 0 {
 		htmlBuff := getHTML(url)
-		links = link.Parse(htmlBuff)
+		links = linklib.Parse(htmlBuff)
 	}
+	validLinks := make([]linklib.Link, 0)
 	for _, link := range links {
 		if haveSeen := seen[link]; haveSeen {
 			continue
 		}
 		seen[link] = true
 		if getURLRoot(link.Href) == baseURL {
+			validLinks = append(validLinks, link)
 			html := getHTML(link.Href)
-			links = append(links, findAllLinks(linklib.Parse(html), seen, currdepth+1)...)
+			validLinks = append(validLinks, buildMap(linklib.Parse(html), seen, currdepth+1)...)
 		}
 	}
-
-	return links
+	return validLinks
 }
 
-// func writeXML()
-// <?xml version="1.0" encoding="UTF-8"?>
-// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-//   <url>
-//     <loc>http://www.example.com/</loc>
-//   </url>
-//   <url>
-//     <loc>http://www.example.com/dogs</loc>
-//   </url>
-// </urlset>
+func toXml(links []linklib.Link) urlset {
+	set := urlset{Xmlns: xmlns}
+	for _, link := range links {
+		set.Urls = append(set.Urls, loc{Value: link.Href})
+	}
+	return set
+}
+
+func writeXML(urls urlset) {
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "  ")
+	if err := enc.Encode(urls); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+}
